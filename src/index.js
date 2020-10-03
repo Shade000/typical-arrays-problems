528e39a1f2b7e058e574b8dae5f8405be4685525
Shade000 <2543631@mail.ru>
2020-10-03 14:28:06 +0300

    docs: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
 exports.min = function min (array) {
-  return 0;
+	if (typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+		return Math.min.apply(null, array);
+	} else {
+		return 0;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+		return Math.max.apply(null, array);
+	} else {
+		return 0;
+	}
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+	if (typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+		function getAvg(array) {
+			const total = array.reduce((acc, c) => acc + c, 0);
+			return total / array.length;
+		}
+		return getAvg(array);
+	} else {
+		return 0;
+	}
 }
